9b8dc4b80736a7b4e63d0ab26b75beca069648bf
ronald <ronald8@gmail.com>
2023-03-14 17:27:26 +0900

    auton

--- 1/src/main.cpp
+++ 1/src/main.cpp
@@ -1,21 +1,32 @@
-/*----------------------------------------------------------------------------*/
-/*                                                                            */
-/*    Module:       main.cpp                                                  */
-/*    Author:       robots20                                                  */
-/*    Created:      Tue Mar 14 2023                                           */
-/*    Description:  V5 project                                                */
-/*                                                                            */
-/*----------------------------------------------------------------------------*/
-
-// ---- START VEXCODE CONFIGURED DEVICES ----
-// ---- END VEXCODE CONFIGURED DEVICES ----
-
 #include "vex.h"
 
 using namespace vex;
 
+controller ctrl;
+competition Comp;
+
+motor fRight (PORT10);
+motor fLeft (PORT3,true);
+motor bRight (PORT20);
+motor bLeft (PORT12,true);
+motor_group cLeft (fLeft, bLeft);
+motor_group cRight (fRight, bRight);
+
 int main() {
-  // Initializing Robot Configuration. DO NOT REMOVE!
-  vexcodeInit();
+
+  cRight.spin (directionType::fwd, 50, velocityUnits::pct);
+  cLeft.spin (directionType::fwd, 50, velocityUnits::pct);
+  task::sleep (4000);
+
+  cRight.spin (directionType::fwd, 20, velocityUnits::pct);
+  cLeft.spin (directionType::fwd, -20, velocityUnits::pct);
+  task::sleep (1500);
+
+  cRight.spin (directionType::fwd, -100, velocityUnits::pct);
+  cLeft.spin (directionType::fwd, -100, velocityUnits::pct);
+  task::sleep (2000);
+
+  cRight.stop();
+  cLeft.stop();
   
 }
